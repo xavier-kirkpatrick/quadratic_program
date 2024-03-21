79c4a48949ea97abe2bb012d0b9d479e56212e6c
Xavier Kirkpatrick <kirkpatrickxavier@gmail.com>
2024-03-21 15:12:29 +1300

    add cout for both sqrt results

--- quadratic.cpp
+++ quadratic.cpp
@@ -7,15 +7,20 @@ int main() {
   double b;
   double c;
 
-  std::cout << "Enter a";
+  std::cout << "Enter a: ";
   std::cin >> a;
 
-  std::cout << "Enter b";
+  std::cout << "Enter b: ";
   std::cin >> b;
 
-  std::cout << "Enter c";
+  std::cout << "Enter c: ";
   std::cin >> c;
 
   double root1 = (-b + std::sqrt(b * b - 4 * a * c)) / (2 * a);
   double root2 = (-b - std::sqrt(b * b - 4 * a * c)) / (2 * a);
+
+  std::cout << "Root 1: " << root1 << std::endl;
+  std::cout << "Root 2: " << root2 << std::endl;
+
+  return 0;
 }
